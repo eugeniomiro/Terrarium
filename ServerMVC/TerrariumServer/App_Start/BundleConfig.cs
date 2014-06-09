34d59de3b2f1e284bc4e195f3dc09e80debde3ab
Eugenio Miró <eugenio.miro@gmail.com>
2014-06-08 21:48:58 -0300

    Bootstrap update

--- ServerMVC/TerrariumServer/App_Start/BundleConfig.cs
+++ ServerMVC/TerrariumServer/App_Start/BundleConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace TerrariumServer
 {
@@ -17,6 +16,14 @@ namespace TerrariumServer
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.unobtrusive*",
                         "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
