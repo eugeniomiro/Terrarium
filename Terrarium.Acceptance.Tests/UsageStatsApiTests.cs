55182494b640cc757883742c2a9ace168038733e
Eugenio Miró <eugenio.miro@neoris.com>
2020-04-17 18:46:31 -0300

    rename controller and extract constant for the name

--- Terrarium.Acceptance.Tests/UsageStatsApiTests.cs
+++ Terrarium.Acceptance.Tests/UsageStatsApiTests.cs
@@ -5,11 +5,13 @@ namespace Terrarium.Acceptance.Tests
     [TestClass]
     public class UsageStatsApiTests
     {
+        private const string RequestUri = "api/UsageStats";
+
         [TestMethod]
         public void GetReturnsResponseWithCorrectStatusCode()
         {
             using (var client = HttpClientFactory.Create()) {
-                var response = client.GetAsync("api/Usage").Result;
+                var response = client.GetAsync(RequestUri).Result;
 
                 Assert.IsTrue(response.IsSuccessStatusCode, $"actual status code: {response.StatusCode}");
             }
--- Terrarium.Web.UI/Controllers/Api/UsageStatsController.cs
+++ Terrarium.Web.UI/Controllers/Api/UsageStatsController.cs
@@ -3,11 +3,11 @@ using System.Web.Http;
 
 namespace Terrarium.Web.UI.Controllers.Api
 {
-    public class UsageController : ApiController
+    public class UsageStatsController : ApiController
     {
         public HttpResponseMessage Get()
         {
-            return this.Request.CreateResponse();
+            return Request.CreateResponse();
         }
     }
 }
