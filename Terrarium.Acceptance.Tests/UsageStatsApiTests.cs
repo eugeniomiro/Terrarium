e3917b343216bf72275198413799d15593d31f3a
Eugenio Miró <eugenio.miro@neoris.com>
2020-04-17 18:00:53 -0300

    exercising what I've learned in Outside In TDD course about acceptance tests

---
+++ Terrarium.Acceptance.Tests/UsageStatsApiTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using System.Web.Http.SelfHost;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TerrariumServer;
+
+namespace Terrarium.Acceptance.Tests
+{
+    [TestClass]
+    public class UsageStatsApiTests
+    {
+        [TestMethod]
+        public void GetReturnsResponseWithCorrectStatusCode()
+        {
+            Uri baseAddress = new Uri("http://localhost:12345");
+
+            using (var config = new HttpSelfHostConfiguration(baseAddress)) {
+                WebApiConfig.Register(config);
+                using (var server = new HttpSelfHostServer(config))
+                using (var client = new HttpClient(server)) {
+                    client.BaseAddress = baseAddress;
+                    var response = client.GetAsync("api/Usage").Result;
+
+                    Assert.IsTrue(response.IsSuccessStatusCode, $"actual status code: {response.StatusCode}");
+                }
+            }
+        }
+    }
+}
---
+++ Terrarium.Web.UI/Controllers/Api/UsageController.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Http;
+using System.Web.Http;
+
+namespace Terrarium.Web.UI.Controllers.Api
+{
+    public class UsageController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            return this.Request.CreateResponse();
+        }
+    }
+}
