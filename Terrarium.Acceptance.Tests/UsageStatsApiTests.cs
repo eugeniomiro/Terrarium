097b3b104301f9e27a268fbced0b28fa0379800a
Eugenio Miró <eugenio.miro@neoris.com>
2020-04-17 18:21:05 -0300

    Extract HttpClient creation helper method

--- Terrarium.Acceptance.Tests/UsageStatsApiTests.cs
+++ Terrarium.Acceptance.Tests/UsageStatsApiTests.cs
@@ -12,17 +12,30 @@ namespace Terrarium.Acceptance.Tests
         [TestMethod]
         public void GetReturnsResponseWithCorrectStatusCode()
         {
-            Uri baseAddress = new Uri("http://localhost:12345");
+            using (var client = CreateHttpClient()) {
+                var response = client.GetAsync("api/Usage").Result;
 
-            using (var config = new HttpSelfHostConfiguration(baseAddress)) {
-                WebApiConfig.Register(config);
-                using (var server = new HttpSelfHostServer(config))
-                using (var client = new HttpClient(server)) {
-                    client.BaseAddress = baseAddress;
-                    var response = client.GetAsync("api/Usage").Result;
+                Assert.IsTrue(response.IsSuccessStatusCode, $"actual status code: {response.StatusCode}");
+            }
+        }
 
-                    Assert.IsTrue(response.IsSuccessStatusCode, $"actual status code: {response.StatusCode}");
-                }
+        private static HttpClient CreateHttpClient()
+        {
+            var baseAddress = new Uri("http://localhost:12345");
+            var config      = new HttpSelfHostConfiguration(baseAddress);
+
+            WebApiConfig.Register(config);
+            var server = new HttpSelfHostServer(config);
+            var client = new HttpClient(server);
+            try {
+                client.BaseAddress = baseAddress ;
+                return client; 
+            } catch {
+                server.Dispose();
+                client.Dispose();
+                throw;
+            } finally {
+                config.Dispose();
             }
         }
     }
